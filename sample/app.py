ab1d8ab01124c763480979c3bc0c2fe9b69d6728
Noriyuki Abe <noriyukipy@gmail.com>
2020-07-07 20:32:34 +0900

    Remove sample directory

--- sample/app.py
+++
@@ -1,9 +0,0 @@
-class MyApp:
-    def __init__(self, service, config):
-        """"""
-        self._service = service
-
-    def handle(self, message):
-        res = f'This is a response to "{message.text}"'
-        message.respond(res)
-        self._service.post(f'This is a post to "{message.text}"')
