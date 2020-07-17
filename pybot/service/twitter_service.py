48d9b11e10ba385af3fd355472a6c03f527dd43f
Noriyuki Abe <noriyukipy@gmail.com>
2020-07-17 18:21:11 +0900

    Modify twitter service to deal with connection error

--- pybot/service/twitter_service.py
+++ pybot/service/twitter_service.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
+from typing import List
 import twitter
 import time
+import requests
+import logging
+logging = logging.getLogger(__file__)
 
 
 class TwitterMessage:
@@ -36,28 +40,45 @@ class TwitterSampleStreamService:
         self._api = api
 
     def get_stream(self):
-        need_sleep = False
-        for status in self._api.GetStreamSample():
-            if "text" not in status:
-                continue
-
-            text = status["text"]
-            cond = (
-                # Check lang
-                "lang" in status and
-                self._config.lang and
-                status["lang"] == self._config.lang and
-                # Check max_len
-                ((not self._config.max_len) or
-                 len(text) <= self._config.max_len)
-            )
-            if cond:
-                yield TwitterMessage(text=text)
-                need_sleep = True
-
-            if need_sleep:
-                time.sleep(self._config.interval)
+        while True:
+            try:
                 need_sleep = False
+                for status in self._api.GetStreamSample():
+                    if "text" not in status:
+                        continue
+
+                    text = status["text"]
+
+                    contains_black_words = False
+                    for bwd in self._config.black_words:
+                        if bwd in text:
+                            contains_black_words = True
+                            break
+                    if contains_black_words:
+                        continue
+
+                    cond = (
+                        # Check lang
+                        "lang" in status and
+                        self._config.lang and
+                        status["lang"] == self._config.lang and
+                        # Check max_len
+                        ((not self._config.max_len) or
+                         len(text) <= self._config.max_len)
+                    )
+                    if cond:
+                        yield TwitterMessage(text=text)
+                        need_sleep = True
+
+                    if need_sleep:
+                        time.sleep(self._config.interval)
+                        need_sleep = False
+        
+            except requests.exceptions.ChunkedEncodingError:
+                logging.info(
+                    "Connection to Twitter was broken."
+                    "Reconnect again"
+                )
 
     def post(self, text):
         raise NotImplementedError()
@@ -71,3 +92,4 @@ class TwitterConfig(BaseModel):
     max_len: int = None
     lang: str = None
     interval: int = None
+    black_words: List[str] = []
