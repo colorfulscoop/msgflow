885797270083862a579ebc7f2e08d6292658a977
Noriyuki Abe <noriyukipy@gmail.com>
2020-12-21 21:14:29 +0900

    Add extras_require for installing test dependent packages

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ setuptools.setup(
         "python-twitter==3.5",
         "slackclient==1.2.1",
         "envyaml==0.2060",
-        "pytest==5.4.3",
     ],
+    extras_require={
+        "test": ["pytest~=5.0", "black==20.8b1"],
+    },
     version="0.2.1",
     author="Noriyuki Abe",
 )
