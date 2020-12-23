693c6b1105c542f9ffd767ac915aab53e860ef09
Noriyuki Abe <noriyukipy@gmail.com>
2020-12-23 20:55:22 +0900

    Implement ContextMemory

---
+++ msgflow/memories.py
@@ -0,0 +1,15 @@
+class ContextMemory:
+    def __init__(self, max_history=None):
+        self._max_history = max_history
+        self._context = dict()
+
+    def add(self, key, msg):
+        ctx = self._context.get(key, []) + [msg]
+
+        if self._max_history and len(ctx) > self._max_history:
+            ctx = ctx[len(ctx) - self._max_history :]
+
+        self._context[key] = ctx
+
+    def get(self, key):
+        return self._context.get(key, [])
