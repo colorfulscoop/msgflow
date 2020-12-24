be0c3e6acc4cb60e47be60a1f0c62bd7209241a2
Noriyuki Abe <noriyukipy@gmail.com>
2020-12-25 00:59:54 +0900

    Modify Message protocol to remove the implementation

--- msgflow/protocol.py
+++ msgflow/protocol.py
@@ -4,15 +4,10 @@ from typing import Protocol
 class Message(Protocol):
     @property
     def text(self) -> str:
-        return self._text
+        raise NotImplementedError()
 
     def respond(self, text: str) -> None:
-        self._api.api_call(
-            "chat.postMessage",
-            channel=self._config.channel,
-            text=f"<@{self._user}> {text}",
-            as_user=True,
-        )
+        raise NotImplementedError()
 
 
 class Service(Protocol):
