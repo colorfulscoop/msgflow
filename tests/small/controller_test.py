afcdb9306952fa37d8a160ac18cc3dcff938c783
Noriyuki Abe <noriyukipy@gmail.com>
2020-12-27 17:56:18 +0900

    update test for controller

--- tests/small/controller_test.py
+++ tests/small/controller_test.py
@@ -1,4 +1,4 @@
-from msgflow.controller import Controller
+from msgflow.controller import Bot
 
 
 class MockMessage:
@@ -10,32 +10,58 @@ class MockMessage:
         return self._text
 
 
-class MockService:
+class MockServicePut:
     def __init__(self, msgs):
         self._msgs = msgs
 
-    def get_stream(self):
-        return self._msgs
+    def start(self, bot):
+        for msg in self._msgs:
+            bot.put_msg(msg)
+
+
+class MockServiceHandle:
+    def __init__(self, msgs):
+        self._msgs = msgs
+
+    def start(self, bot):
+        for msg in self._msgs:
+            bot.handle_msg(msg)
 
 
 class MockApp:
     def __init__(self):
         self.accepted_msg = []
 
-    def handle(self, msg):
+    def handle(self, bot, msg):
         self.accepted_msg.append(msg)
 
 
-def test_controller():
+def test_controller_put():
     msgs = [
         MockMessage(text="1"),
         MockMessage(text="2"),
         MockMessage(text="3"),
     ]
 
-    svc = MockService(msgs=msgs)
+    svc = MockServicePut(msgs=msgs)
     app = MockApp()
-    controller = Controller(service=svc, app=app)
+    controller = Bot(service=svc, post_service=svc, app=app)
+
+    controller.start()
+
+    assert app.accepted_msg == msgs
+
+
+def test_controller_handle():
+    msgs = [
+        MockMessage(text="1"),
+        MockMessage(text="2"),
+        MockMessage(text="3"),
+    ]
+
+    svc = MockServiceHandle(msgs=msgs)
+    app = MockApp()
+    controller = Bot(service=svc, post_service=svc, app=app)
 
     controller.start()
 
