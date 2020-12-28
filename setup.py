0ed602b78b44035df4238d4bbfd95f6f51f94eb9
Noriyuki Abe <noriyukipy@gmail.com>
2020-12-28 17:03:50 +0900

    Modify setup.py to specify the correct version reference

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setuptools.setup(
         "pydantic~=1.0",
     ],
     extras_require={
-        "test": ["pytest~=5.0", "black~=20.0"],
+        "test": ["pytest~=5.0", "black==20.8b1"],
         "webapi": ["fastapi~=0.63.0", "uvicorn~=0.13.0"],
         "twitter": ["python-twitter==3.5"],
-        "slack": ["slackclient~=1.2.0"],
+        "slack": ["slackclient==1.2.1"],
     },
     version="0.2.1",
     author="Noriyuki Abe",
