64931671b482cd0d5366184f3cc6254be82b1f27
Noriyuki Abe <noriyukipy@gmail.com>
2023-01-30 01:03:08 +0900

    Revise pipeline

--- msgflow/pipeline.py
+++ msgflow/pipeline.py
@@ -1,5 +1,9 @@
 import asyncio
-import sys
+from pydantic import BaseModel
+from msgflow import Channel
+from typing import Any, Optional
+import re
+import inspect
 
 
 class PipelineChannelMixin:
@@ -12,67 +16,69 @@ class PipelineChannelMixin:
 
 
 class Pipeline:
-    handler_enabled = "_pipeline_handler_enabled"
-    handler_service_name = "_pipeline_service_name"
-    handler_filter = "_pipeline_filter"
+    def __init__(self, app):
+        # List up all methods which has "config" attr set by `handler` decorator
+        handlers = []
+        for method_name in dir(app):
+            method = getattr(app, method_name)
+            if hasattr(method, "config"):
+                handlers.append(method)
 
-    def __init__(self, handler):
-        self._handler = handler
+        print(handlers)
+        self._handlers = handlers
 
-        # List up all methods with `_msgflow_handler` tag
-        #print("List up methods")
-        #for method in dir(handler):
-        #    print(method)
-        #    print(getattr(handler, method))
-        #print()
-
-        # List up all methods with `_msgflow_handler` tag
-        self._handle_methods = [
-            getattr(handler, method) for method in dir(handler)
-            if hasattr(getattr(handler, method), self.handler_enabled)
-        ]
-        print(self._handle_methods)
-
-        services = []
-
-        if isinstance(handler.input_channel, dict):
+        # Create a list including input channels
+        input_channels = []
+        if isinstance(app.input_channel, dict):
             # Sort may be needed
-            for svc_name, svc in handler.input_channel.items():
+            for svc_name, svc in app.input_channel.items():
                 svc.set_name(svc_name)
-                services.append(svc)
-        elif isinstance(handler.input_channel, list):
-            services = handler.input_channel
+                input_channels.append(svc)
+        elif isinstance(app.input_channel, list):
+            input_channels = app.input_channel
         else:
-            services = [handler.input_channel]
+            input_channels = [app.input_channel]
 
-        self._services = services
+        self._input_channels = input_channels
 
     async def _handle(self, queue):
         while True:
             msg = await queue.get()
-            for method in self._handle_methods:
-                handler_enabled, handler_service_name, handler_filter = (
-                    getattr(method, self.handler_enabled),
-                    getattr(method, self.handler_service_name),
-                    getattr(method, self.handler_filter)
-                )
+            for handler in self._handlers:
                 # Logging
-                print(f"method: {method.__name__}, enabled: {handler_enabled}, service_name: {handler_service_name}, filter: {str(handler_filter)}")
+                print(f"handler: {handler.__name__}, config: {handler.config}")
 
-                if not handler_enabled:
+                if not handler.config.enabled:
                     continue
-                if (handler_service_name is not None) and (msg.channel.name != handler_service_name):
+                if (handler.config.channel_name is not None) and (msg.channel.name != handler.config.channel_name):
                     continue
-                if (handler_filter is not None) and (not handler_filter(msg)):
+                if (handler.config.cond is not None) and (not handler.config.cond(msg)):
                     continue
-                await method(msg)
+
+                match = None
+                if handler.config.pattern is not None:
+                    match = re.match(handler.config.pattern, handler.config.pattern_attr(msg))
+                    if match is None:
+                        continue
+
+                # Get handlers argument names
+                handler_arg_names = inspect.getfullargspec(handler).args[1:]  # use [1:] to remove first "self"
+
+                # Set up argument to pass to handler
+                args = {"msg": msg}
+                if "match" in handler_arg_names:
+                    args["match"] = match
+                await handler(**args)
+
+                # If one of handlers matches, other handlers will be ignored
+                #break
 
     async def run(self):
         queue = asyncio.Queue()
 
         # Create tasks and **schedule to run soon concurrently**
         service_tasks = []
-        for service in self._services:
+        for service in self._input_channels:
             svc_task = asyncio.create_task(service.flow(queue=queue))
             #svc_task.set_name(f"{svc_task.get_name()}-{service.name}")
             service_tasks.append(svc_task)
@@ -90,9 +96,17 @@ def _default_pattern_attr(msg):
     raise NotImplementedError("Specify which attribute to use for pattern match")
 
 
+class HandlerConfig(BaseModel):
+    enabled: bool
+    channel_name: Optional[str]
+    cond: Optional[Any]
+    pattern: Optional[str]
+    pattern_attr: Any = _default_pattern_attr
+
+
 def handler(
     _func=None, *,
-    service=None,
+    channel=None,
     cond=None,
     pattern=None,
     pattern_attr=_default_pattern_attr,
@@ -101,9 +115,14 @@ def handler(
     """
 
     def decorator(func):
-        setattr(func, Pipeline.handler_enabled, True)
-        setattr(func, Pipeline.handler_service_name, service)
-        setattr(func, Pipeline.handler_filter, cond)
+        # Set "config" attribute
+        func.config = HandlerConfig(
+            enabled=True,
+            channel_name=channel,
+            cond=cond,
+            pattern=pattern,
+            pattern_attr=pattern_attr,
+        )
         return func
 
     if _func is None:
